ceaef7f576d3c169f282d37e79211f7b020290ee
nicollasGomez <gommezthereal@gmail.com>
2024-04-01 07:41:48 -0300

    setimo commit do app/versao mobile finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ export default function App() {
         <div className="bg-laranja p-12 rounded-t-lg">
           <img src={sedan} alt="Icone de carros sedan" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-deca  mb-7">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank" className="font-deca">Ver Mais</a>
+          <p className="font-deca mb-7 text-branco-off">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank" className="font-deca bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-laranja">Ver Mais</a>
         </div>
         <div className="bg-verde p-12">
           <img src={suv} alt="Icone de carros SUV" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">SUVs</h2>
-          <p className="font-deca mb-7">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a href="https://www.webmotors.com.br/catalogo/carroceria/suv/" target="_blank" className="font-deca">Ver Mais</a>
+          <p className="font-deca mb-7 text-branco-off">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/catalogo/carroceria/suv/" target="_blank" className="font-deca bg-offwhitebg px-10 py-3 block w-fit rounded-3xl text-verde">Ver Mais</a>
 
         </div>
         <div className="bg-verde-escuro p-12 rounded-b-lg">
           <img src={luxury} alt="Icone de carros Luxuosos" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
-          <p className="font-deca mb-7">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank" className="font-deca">Ver Mais</a> 
+          <p className="font-deca mb-7 text-branco-off">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank" className="font-deca bg-offwhitebg px-10 py-3 block w-fit rounded-3xl text-verde-escuro">Ver Mais</a> 
           
         </div>
       </section>
