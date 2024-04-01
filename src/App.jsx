732774bbb4a0c5fc543a8fe0c1433d1fe223104e
nicollasGomez <gommezthereal@gmail.com>
2024-04-01 07:10:50 -0300

    quarto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ export default function App() {
       <section>
         <div className="bg-laranja">
           <img src={sedan} alt="Icone de carros sedan" />
-          <h2>Sedans</h2>
-          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <h2 className="font-shoulders">Sedans</h2>
+          <p className="font-deca">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/catalogo/carroceria/sedan/" target="_blank">Ver Mais</a>
         </div>
         <div className="bg-verde">
           <img src={suv} alt="Icone de carros SUV" />
-          <h2>Suvs</h2>
-          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <h2 className="font-shoulders">Suvs</h2>
+          <p className="font-deca">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/catalogo/carroceria/suv/" target="_blank">Ver Mais</a>
 
         </div>
         <div className="bg-verde-escuro">
           <img src={luxury} alt="Icone de carros Luxuosos" />
-          <h2>Luxuosos</h2>
-          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <h2 className="font-shoulders">Luxuosos</h2>
+          <p className="font-deca">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
           <a href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank">Ver Mais</a> 
           
         </div>
