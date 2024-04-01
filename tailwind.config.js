217fe2b41b258c2f4d6f7dbbf36d1b3e0b54cfa1
nicollasGomez <gommezthereal@gmail.com>
2024-04-01 07:00:25 -0300

    instalação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'laranja': '#e28625',
+      'verde':'#006971',
+      'verde-escuro':'#004140',
+      'branco-off':'rgba(255,255,255,0.75)',
+      'white':'#FFF',
+      'offwhitebg':'#f2f2f2',
+    }
+  },
+  plugins: [],
+}
